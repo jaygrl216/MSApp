1c3ec8c5ce093da036d4b4c43a8c35d39a7217e8
Jada <jwashin7@terpmail.umd.edu>
2017-04-30 19:43:18 -0400

    Connected Welcome Activity to other tests

--- MSApp/app/src/main/java/com/example/msapp/KeyActivity.java
+++ MSApp/app/src/main/java/com/example/msapp/KeyActivity.java
@@ -2,12 +2,18 @@ package com.example.msapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class KeyActivity extends AppCompatActivity {
+    private TextView instructions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_key);
+
+        instructions = (TextView) findViewById(R.id.instructions);
+        instructions.setText("Please rotate your phone to start the test.");
+
     }
 }
--- MSApp/app/src/main/java/com/example/msapp/WelcomeActivity.java
+++ MSApp/app/src/main/java/com/example/msapp/WelcomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.msapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void outsideTest(View v) {
+        Intent i = new Intent(this, MapsActivity.class);
+        startActivity(i);
+    }
+
+    public void keyTest(View v) {
+        Intent i = new Intent(this, KeyActivity.class);
+        startActivity(i);
     }
 }
