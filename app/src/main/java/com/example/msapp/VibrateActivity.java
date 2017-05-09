001786f09ff16c49dfbbd6026a8ac94a45c084a8
Jada <jwashin7@terpmail.umd.edu>
2017-05-08 20:44:35 -0400

    added start button

--- app/src/main/java/com/example/msapp/VibrateActivity.java
+++ app/src/main/java/com/example/msapp/VibrateActivity.java
@@ -2,6 +2,7 @@ package com.example.msapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class VibrateActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class VibrateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vibrate);
+    }
+
+    public void startVibrate(View v) {
+
     }
 }
