90cb680f4ac1aa8b7680af7c10db2c4fdfe7cd1a
Jada <jwashin7@terpmail.umd.edu>
2017-05-08 20:52:36 -0400

    Added vibrate permission request

--- app/src/main/java/com/example/msapp/VibrateActivity.java
+++ app/src/main/java/com/example/msapp/VibrateActivity.java
@@ -1,8 +1,16 @@
 package com.example.msapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class VibrateActivity extends AppCompatActivity {
 
@@ -13,6 +21,29 @@ public class VibrateActivity extends AppCompatActivity {
     }
 
     public void startVibrate(View v) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.VIBRATE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.VIBRATE},
+                    1);
+        }
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        // Make sure it's our original READ_CONTACTS request
+        if (requestCode == 1) {
+            if (grantResults.length == 1 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Vibrate permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Vibrate permission denied", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+
 }
