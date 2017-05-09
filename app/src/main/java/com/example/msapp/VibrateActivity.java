0cd0022653c4b2533b8b1d3d0d5377c0db9733c3
Jada <jwashin7@terpmail.umd.edu>
2017-05-08 22:22:09 -0400

    12

--- app/src/main/java/com/example/msapp/VibrateActivity.java
+++ app/src/main/java/com/example/msapp/VibrateActivity.java
@@ -12,6 +12,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class VibrateActivity extends AppCompatActivity {
@@ -24,12 +25,16 @@ public class VibrateActivity extends AppCompatActivity {
 
 
     private Vibrator vibrator;
+    private Button touch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vibrate);
 
         vibrator = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+        touch = (Button) findViewById(R.id.start);
+
     }
 
     public void startVibrate(View v) {
@@ -39,6 +44,7 @@ public class VibrateActivity extends AppCompatActivity {
         } else {
             System.out.println("Permission Granted");
         }
+        touch.setText("Stop");
 
         runTest();
     }
